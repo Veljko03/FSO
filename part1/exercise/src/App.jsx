4f0200a80e975fa00ea9314ef579731f1490a9db
Veljko03 <veljkodovragovic@gmail.com>
2024-05-30 00:29:28 +0200

    more practitcing c part done

--- part1/exercise/src/App.jsx
+++ part1/exercise/src/App.jsx
@@ -13,17 +13,35 @@ const Hello = ({ name, age }) => {
   );
 };
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-  const handleClick = () => {
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
     setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
   };
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 };
