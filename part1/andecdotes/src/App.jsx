ad86300b51cce3494b9895e9277c204e031761ad
Veljko03 <veljkodovragovic@gmail.com>
2024-05-31 23:46:51 +0200

    finishing

---
+++ part1/andecdotes/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const MostVoted = ({ max, anecdote }) => {
+  if (max != 0) {
+    return (
+      <div>
+        {anecdote}
+        <p>it has {max} votes</p>
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Uint8Array(8));
+  const randomQuote = () => {
+    const ranNum = Math.floor(Math.random() * 8);
+
+    setSelected(ranNum);
+  };
+
+  const vote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  let max = Math.max(...votes);
+
+  let index = votes.indexOf(max);
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {votes[selected]}</p>
+      <div>
+        <button onClick={vote}>Vote</button>
+        <button onClick={randomQuote}>next anecdotes</button>
+      </div>
+      <h1>Anecdote with the most votes</h1>
+      <MostVoted max={max} anecdote={anecdotes[index]} />
+    </div>
+  );
+};
+
+export default App;
