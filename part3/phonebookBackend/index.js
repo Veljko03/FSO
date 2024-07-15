47c0ab72450820bea088f72491d2f28c6ba01ec7
Veljko03 <veljkodovragovic@gmail.com>
2024-07-15 17:20:19 +0200

    phonebook backend done

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("tiny"));
+
 app.use(express.json());
+
+morgan.token("object", (request) => {
+  return JSON.stringify(request.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :object"
+  )
+);
 
 let allPersons = [
   {
